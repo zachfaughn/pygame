c063e77170df8f7c3dac98e69c4ac00181f23a0b
zachfaughn <zachfaughn@gmail.com>
2024-12-12 19:42:05 -0600

    added play rotation

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     dt = 0
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 
