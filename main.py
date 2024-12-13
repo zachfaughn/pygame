f1757a51daa573377e69ecc5eec78274333dbb95
zachfaughn <zachfaughn@gmail.com>
2024-12-12 19:02:34 -0600

    added some FPS Logic

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import pygame
 from constants import *
 
 def main():
+    dt = 0
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         screen.fill("black")
         pygame.display.flip()
+
+
+        dt = clock.tick(60) / 1000
     
 
 
 if __name__ == "__main__":
     main()
+
